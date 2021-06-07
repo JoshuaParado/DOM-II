21255a263c99e0f506c8eceb6e96359620706753
Joshua Parado <jpa3ado@gmail.com>
2021-06-07 00:28:23 -0400

    done

--- js/index.js
+++ js/index.js
@@ -1,1 +1,17 @@
-// Your code goes here
+const busImage = document.getElementsByTagName("img")[0]
+busImage.addEventListener("mouseenter", () => {
+    busImage.style.transform = "scale(1.2)";
+    busImage.style.transition = "all 0.3s"
+})
+
+const headerTwo = document.getElementsByTagName("h2")[0]
+headerTwo.addEventListener("mouseover", () => {
+    headerTwo.style.transform = "scale(1.2)";
+    headerTwo.style.transition = "all 0.3s"
+})
+
+const secondHeader = document.getElementsByTagName("h2")[1]
+secondHeader.addEventListener("keypress", () => {
+    secondHeader.style.transform = "scale(1,2)";
+    secondHeader.style.transition = "all 0.5s"
+})
